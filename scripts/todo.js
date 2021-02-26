b2d5e0579cdb7d716e5fce5ee69fce8b3d27e4ee
Christian Siegel <christian.siegel@gmx.net>
2021-02-26 08:14:46 +0100

    add zalando support table

---
+++ scripts/todo.js
@@ -0,0 +1,33 @@
+// can be used to create the rule support markdown table
+
+https = require('https');
+fs = require('fs');
+
+(async () => {
+  const testFiles = await fs.promises.readdir(process.cwd() + '/tests');
+  const testedIds = testFiles
+    .map((filename) => filename.match(/^([0-9]{3})-/))
+    .filter((m) => m)
+    .map((m) => '#' + m[1]);
+  https.get('https://opensource.zalando.com/restful-api-guidelines/rules.json', (res) => {
+    let body = '';
+    res.on('data', (chunk) => (body += chunk));
+    res.on('end', () => {
+      const rules = JSON.parse(body).rules;
+      let markdown = '<!-- table created with `node scripts/todo.js` -->\n';
+      markdown += '| id | title |supported |\n';
+      markdown += '| --- | --- | --- |\n';
+      rules.forEach((rule) => {
+        const tested = testedIds.indexOf(rule.id) != -1;
+        markdown += `| [${rule.id}][${rule.id}] | [${rule.title}][${rule.id}] | ${
+          tested ? ':heavy_check_mark:' : '-'
+        } |\n`;
+      });
+      markdown += '\n';
+      rules.forEach((rule) => {
+        markdown += `[${rule.id}]: https://opensource.zalando.com/restful-api-guidelines/${rule.id}\n`;
+      });
+      console.log(markdown);
+    });
+  });
+})();
