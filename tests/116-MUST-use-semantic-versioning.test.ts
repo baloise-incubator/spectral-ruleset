b3020f5fab6598a12d270b117b3a0163e4b7253d
Dane Leube <dane.leube@baloise.ch>
2021-08-13 09:23:50 +0200

    feat(semantic-versioning): Allow literals as postfix in semantic-version

--- tests/116-MUST-use-semantic-versioning.test.ts
+++ tests/116-MUST-use-semantic-versioning.test.ts
@@ -9,7 +9,31 @@ describe('MUST use semantic versioning [116]', () => {
     expect(result).toEqual([
       expect.objectContaining({
         code: 'must-use-semantic-versioning',
-        message: '`version` property should match pattern `^[0-9]+\\.[0-9]+\\.[0-9]+$`',
+        message: '`version` property should match pattern `^[0-9]+\\.[0-9]+\\.[0-9]+(-[0-9a-zA-Z-]+(\\.[0-9a-zA-Z-]+)*)?$`',
+        severity: DiagnosticSeverity.Error,
+      }),
+    ]);
+  });
+  test('Detect if `version` does match SemVer pattern', async () => {
+    const openApi = await loadOpenApiSpec('base-openapi.yml');
+    openApi.info.version = '47.11.1';
+    const result = await lint(openApi);
+    expect(result).toEqual([]);
+  });
+  test('Detect if `version` does match SemVer pattern with snapshot', async () => {
+    const openApi = await loadOpenApiSpec('base-openapi.yml');
+    openApi.info.version = '47.11.1-SNAPSHOT';
+    const result = await lint(openApi);
+    expect(result).toEqual([]);
+  });
+  test('Detect if `version` does not match SemVer pattern with snapshot', async () => {
+    const openApi = await loadOpenApiSpec('base-openapi.yml');
+    openApi.info.version = '47.11-SNAPSHOT';
+    const result = await lint(openApi);
+    expect(result).toEqual([
+      expect.objectContaining({
+        code: 'must-use-semantic-versioning',
+        message: '`version` property should match pattern `^[0-9]+\\.[0-9]+\\.[0-9]+(-[0-9a-zA-Z-]+(\\.[0-9a-zA-Z-]+)*)?$`',
         severity: DiagnosticSeverity.Error,
       }),
     ]);
