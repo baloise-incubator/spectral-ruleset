bc9a581bfa2911f9477404769eaafcb3e29ef633
David Weber <david.weber@w3tec.ch>
2022-02-10 08:26:46 +0100

    refactor: implement review feedback

--- functions/validate-b3-tracing.js
+++ functions/validate-b3-tracing.js
@@ -14,7 +14,7 @@ module.exports = (targetValue) => {
       param.name && (param.name.toLowerCase() === 'x-b3-traceid' || param.name.toLowerCase() === 'x-b3-spanid'),
   );
 
-  if (b3Params.length < 2 || !b3Params.every((param) => param.in === 'header' && param.required === true)) {
+  if (b3Params.length !== 2 || !b3Params.every((param) => param.in === 'header' && param.required === true)) {
     return [
       {
         message: `B3 header X-B3-Traceid or X-B3-Spanid missing`,
